b320c39f7c614761211a5a123a6a433fa7b73589
Hisham70900 <hishammuhammed085@gmail.com>
2026-07-31 20:10:25 +0530

    Create customers table

---
+++ SQL/02_tables.sql
@@ -0,0 +1,13 @@
+-- ============================================
+-- Lesson 2: Create Customers Table
+-- ============================================
+
+USE ecommerce_analytics;
+
+CREATE TABLE customers (
+    customer_id VARCHAR(50) PRIMARY KEY,
+    customer_unique_id VARCHAR(50) NOT NULL,
+    customer_zip_code_prefix INT,
+    customer_city VARCHAR(100),
+    customer_state CHAR(2)
+);
